22df071df8915adfab6e8d81e62fa6c731004478
Andrii Makohin <andreas2010@ukr.net>
2020-09-14 12:58:32 +0300

    add trs

--- src/Components/AddTransaction/AddTransaction.js
+++ src/Components/AddTransaction/AddTransaction.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useEffect,createRef} from "react";
+import PropTypes from 'prop-types';
+import AddTransactionForm from './AddTransactionForm/AddTransactionForm';
+import styles from './ModalAddTransaction.module.css';
+
 
 const AddTransaction = () => {
   return (
-    <div>
-      <h1>AddTransaction</h1>
-    </div>
+    <div
+        className={styles.backdrop}
+        ref={this.backdropRef}
+        onClick={this.handleBackdropClick}
+        role="presentation"
+      >
+        <div className={styles.modal}>
+          <AddTransactionForm
+            closeModalAddTransaction={closeModalAddTransaction}
+            addTransaction={addTransaction}
+          />
+        </div>
+      </div>
   );
 };
 
